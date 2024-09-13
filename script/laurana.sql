a7b57c59853c9bad5894c9b8edd956ecf33f3bb9
Diego Gutierrez da Silva <diego.gutierrezdasilva@gmail.com>
2024-09-13 15:00:39 -0300

    varchar

--- script/laurana.sql
+++ script/laurana.sql
@@ -192,7 +192,7 @@ CREATE TABLE `StatusBaseConhecimentoDocumento` (
 
 CREATE TABLE `Notificacao` (
   `IdNotificacao` integer PRIMARY KEY,
-  `IdUsuario` varcgar(36) NOT NULL,
+  `IdUsuario` varchar(36) NOT NULL,
   `Mensagem` varchar(50) NOT NULL,
   `DataCriacaoNotificacao` datetime NOT NULL,
   `DataEntregaNotificacao` datetime,
